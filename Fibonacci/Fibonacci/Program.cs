dab6e2b4e636eee595fca531b20012ace077cabb
praveen dodla <dodlapraveen09@gmail.com>
2022-08-06 20:19:57 +0530

    yeasrady tasks

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+//0 
+namespace Fibonacci
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+            
+        {
+            int firstNumber = 0, SecondNumber = 1, nextNumber, numberOfElements;
+            Console.Write("Enter the number of elements to Print : "); //
+            numberOfElements = int.Parse(Console.ReadLine());
+            if (numberOfElements < 2)
+            {
+                Console.Write("Please Enter a number greater than two");//i will enter 2
+            }
+            else
+            {
+                //First print first and second number
+
+                Console.Write(firstNumber + " " + SecondNumber + " "); // 0 1 
+
+                //Starts the loop from 2 because 0 and 1 are already printed
+
+                for (int i = 2; i < numberOfElements; i++)
+                {
+                    nextNumber = firstNumber + SecondNumber; // 1
+                    Console.Write(nextNumber + " "); 
+                    firstNumber = SecondNumber;
+                    SecondNumber = nextNumber;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, x = 0, f = 0;
+            Console.Write("Enter a Number: ");
+            num = int.Parse(Console.ReadLine()); 
+            x = num / 2;
+            for (i = 2; i <= x; i++) 
+            {
+                if (num % i == 0)
+                {
+                    Console.Write("Number is not Prime.");
+                    f = 1;
+                    break;
+                }
+            }
+            if (f == 0)
+                Console.Write("Number is Prime.");
+        }
+    }
+}
+
+//A Prime number is a number that can be divided either by itself or 1
+//2 prime  1 and 2   ex: 2*1=2 ,1*2=2
+//4 not prime 1,2,4  ex  1*4=4 , 2*2=4 , 4*1=4
